75b1d607423ccbb1fd88b6e84276fff0a91f733b
gustavomullerf <gustavoeduardomf@gmail.com>
2023-05-09 21:12:32 -0300

    Reformulando Enums

--- src/main/java/br/com/uniamerica/estacionamento/entity/Cor.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Cor.java
@@ -1,5 +1,20 @@
 package br.com.uniamerica.estacionamento.entity;
 
 public enum Cor {
-    BRANCO, PRATA, PRETO, CINZA, VERMELHO, AZUL, BEGE, VERDE
+    BRANCO(1, "Branco"),
+    PRATA(2, "Prata"),
+    PRETO(3, "Preto"),
+    CINZA(4, "Cinza"),
+    VERMELHO(5, "Vermelho"),
+    AZUL(6, "Azul"),
+    BEGE(7, "Bege"),
+    VERDE(8, "Verde");
+
+    public final int valorNumerico;
+    public final String valorString;
+
+    private Cor(int valorNumerico, String valorString){
+        this.valorNumerico=valorNumerico;
+        this.valorString=valorString;
+    }
 }
--- src/main/java/br/com/uniamerica/estacionamento/entity/Tipo.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Tipo.java
@@ -1,5 +1,15 @@
 package br.com.uniamerica.estacionamento.entity;
 
 public enum Tipo {
-    CARRO, MOTO, VAN
+    CARRO(1, "Carro"),
+    MOTO(2, "Moto"),
+    VAN(3, "Van");
+
+    public final int valorNumerico;
+    public final String valorString;
+
+    private Tipo(int valorNumerico, String valorString){
+        this.valorNumerico=valorNumerico;
+        this.valorString=valorString;
+    }
 }
