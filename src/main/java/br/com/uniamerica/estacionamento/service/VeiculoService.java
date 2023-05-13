9b28b6ec807cdcde02596a6c32d13c5100fe62e1
gustavomullerf <gustavoeduardomf@gmail.com>
2023-05-13 16:04:30 -0300

    Padronizando VeiculoService

--- src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
@@ -14,34 +14,34 @@ public class VeiculoService {
     @Transactional
     public void cadastraVeiculo(Veiculo veiculo){
         if(veiculo.getPlaca().isEmpty()){
-            throw new RuntimeException("O campo placa não pode ser nulo");
+            throw new RuntimeException("o campo placa não pode ser nulo");
         }
         if(veiculoRepository.findByPlaca(veiculo.getPlaca()) != null){
-            throw new RuntimeException("O campo placa já existe");
+            throw new RuntimeException("o campo placa já existe");
         }
         if(veiculo.getModelo() == null){
-            throw new RuntimeException("O campo modelo não pode ser nulo");
+            throw new RuntimeException("o campo modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getNome().isEmpty()){
-            throw new RuntimeException("O campo nome do modelo não pode ser nulo");
+            throw new RuntimeException("o campo nome do modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getMarca() == null){
-            throw new RuntimeException("O campo marca do modelo não pode ser nulo");
+            throw new RuntimeException("o campo marca do modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getMarca().getNome().isEmpty()){
-            throw new RuntimeException("O campo nome da marca do modelo não pode ser nulo");
+            throw new RuntimeException("o campo nome da marca do modelo não pode ser nulo");
         }
         if(veiculo.getCor() == null){
-            throw new RuntimeException("O campo cor não pode ser nulo");
+            throw new RuntimeException("o campo cor não pode ser nulo");
         }
         if(veiculo.getTipo() == null){
-            throw new RuntimeException("O campo tipo não pode ser nulo");
+            throw new RuntimeException("o campo tipo não pode ser nulo");
         }
         if(veiculo.getAno() == 0){
-            throw new RuntimeException("O campo ano não pode ser zero");
+            throw new RuntimeException("o campo ano não pode ser zero");
         }
         if(veiculo.getPlaca().length() > 7){
-            throw new RuntimeException("A placa do condutor excede o máximo de caracteres (7)");
+            throw new RuntimeException("a placa do condutor excede o máximo de caracteres (7)");
         }
         this.veiculoRepository.save(veiculo);
     }
@@ -50,37 +50,37 @@ public class VeiculoService {
     public void atualizaVeiculo(final Long id, Veiculo veiculo){
         final Veiculo veiculoBanco = this.veiculoRepository.findById(id).orElse(null);
         if(veiculoBanco==null || !veiculoBanco.getId().equals(veiculo.getId())){
-            throw new RuntimeException("Não foi possivel encontrar o registro informado");
+            throw new RuntimeException("não foi possivel encontrar o registro informado");
         }
         if(veiculo.getPlaca().isEmpty()){
-            throw new RuntimeException("O campo placa não pode ser nulo");
+            throw new RuntimeException("o campo placa não pode ser nulo");
         }
         if(veiculoRepository.findByPlaca(veiculo.getPlaca()) != null){
-            throw new RuntimeException("O campo placa já existe");
+            throw new RuntimeException("o campo placa já existe");
         }
         if(veiculo.getModelo() == null){
-            throw new RuntimeException("O campo modelo não pode ser nulo");
+            throw new RuntimeException("o campo modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getNome().isEmpty()){
-            throw new RuntimeException("O campo nome do modelo não pode ser nulo");
+            throw new RuntimeException("o campo nome do modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getMarca() == null){
-            throw new RuntimeException("O campo marca do modelo não pode ser nulo");
+            throw new RuntimeException("o campo marca do modelo não pode ser nulo");
         }
         if(veiculo.getModelo().getMarca().getNome().isEmpty()){
-            throw new RuntimeException("O campo nome da marca do modelo não pode ser nulo");
+            throw new RuntimeException("o campo nome da marca do modelo não pode ser nulo");
         }
         if(veiculo.getCor() == null){
-            throw new RuntimeException("O campo cor não pode ser nulo");
+            throw new RuntimeException("o campo cor não pode ser nulo");
         }
         if(veiculo.getTipo() == null){
-            throw new RuntimeException("O campo tipo não pode ser nulo");
+            throw new RuntimeException("o campo tipo não pode ser nulo");
         }
         if(veiculo.getAno() == 0){
-            throw new RuntimeException("O campo ano não pode ser zero");
+            throw new RuntimeException("o campo ano não pode ser zero");
         }
         if(veiculo.getPlaca().length() > 7){
-            throw new RuntimeException("A placa do condutor excede o máximo de caracteres (7)");
+            throw new RuntimeException("a placa do condutor excede o máximo de caracteres (7)");
         }
         this.veiculoRepository.save(veiculo);
     }
